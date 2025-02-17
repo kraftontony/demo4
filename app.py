ac291ed7273be7a6edae1b9b2132fb24820a7c87
kraftontony <kraftontony@gmail.com>
2025-02-17 20:43:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,11 +11,14 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('sample-data/diabetes_model.sav', 'rb'))
+# diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
+heart_disease_model = pickle.load(open('sample-data/heart_disease_model.sav', 'rb'))
+# heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
 
-parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('sample-data/parkinsons_model.sav', 'rb'))
+# parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
 # sidebar for navigation
 with st.sidebar:
